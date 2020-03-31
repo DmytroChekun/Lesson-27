2b3227f9a2372c76da0c0b648c8d1e8ac7ae326d
DmytroChekun <snashd4@gmail.com>
2020-03-31 17:22:42 +0300

    finished

--- js/shoppingList.js
+++ js/shoppingList.js
@@ -52,6 +52,17 @@ if(!itemExists) {
         bought: false
     });
 }
+
+shoppingList.sort ((first, second) => {
+    if ( second.bought == first.bought ) {
+        return 0;
+    } else if ( second.bought ) {
+        return -1;
+    } else {
+        return 1;
+    }
+});
+
 let shoppingListDisplay = function(){
     shoppingList.forEach ( function( item, index, array ) {
         if (item.bought == false){
@@ -61,13 +72,11 @@ let shoppingListDisplay = function(){
             document.write ( "<li>" +item.name+ ': ' +item.amount+ "</li>" );
 
         }
-        shoppingList.sort ((prev, next) => {
-            if ( prev.bought < next.bought ) return -1;
-            if ( prev.bought < next.bought ) return 1;
-        });
-
     });
 };
+
+//                                                Це останній пункт завдання. Працює не коректно. Відмічає товар як куплений тільки в тому випадку, коли я ввожу їх по порядку списка.
+
 // let markAsBought = '';
 // shoppingList.forEach ( function( item, index, array ) {
 //     markAsBought = prompt( "enter the name of product that you allready buy" );
